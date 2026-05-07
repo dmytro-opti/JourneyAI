f7f716e9ebf78a17dc04c42903da2414b98240db
kaianyo <luninamarina.1984@gmail.com>
2026-05-08 00:38:21 +0300

    added UserModel.cs

---
+++ TravellerAI.Domain/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TravellerAI.Domain.Models;
+
+public class UserModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    
+}
